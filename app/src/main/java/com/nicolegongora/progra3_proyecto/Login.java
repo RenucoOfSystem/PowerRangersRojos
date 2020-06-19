27d6cc779b6d58c8b17f2fa7c1e7adf3b88bace8
Renucopata <r.rengel99@gmail.com>
2020-06-19 08:14:44 -0400

    Debug

---
+++ app/src/main/java/com/nicolegongora/progra3_proyecto/Login.java
@@ -0,0 +1,16 @@
+package com.nicolegongora.progra3_proyecto;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
--- app/src/main/java/com/nicolegongora/progra3_proyecto/MainActivity.java
+++ app/src/main/java/com/nicolegongora/progra3_proyecto/MainActivity.java
@@ -2,13 +2,25 @@ package com.nicolegongora.progra3_proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
+    public void toSignIn(View view){
+        Intent i = new Intent(this, Login.class);
+        startActivity(i);
+    }
+
+    public void toSignUp(View view){
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
+    }
+
 }
